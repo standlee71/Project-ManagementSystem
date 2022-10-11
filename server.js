1d88a53d9f6c4df113a630133cd925913f7f5302
standlee71 <standlee@naver.com>
2022-10-11 16:23:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 // 위 코드는 node.js express를 사용할 때 일반적으로 작성하는 코드
+// 테스트로 수정해서 push 설정
 
 //---- DT CONNECTION -----//
 const fs = require('fs');
